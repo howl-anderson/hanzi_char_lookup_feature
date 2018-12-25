1f6941a002137f5bf71494fbc7cf9c30c07477b3
Xiaoquan Kong <u1mail2me@gmail.com>
2018-12-25 17:13:07 +0800

    Bugfix: package missing files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hanzi_char_lookup_feature',
     version='0.1',
-    packages=['hanzi_char_lookup_feature'],
+    packages=['hanzi_char_lookup_feature', 'hanzi_char_lookup_feature.n_gram_lookup', 'hanzi_char_lookup_feature.n_gram_match'],
     url='',
     license='',
     author='Xiaoquan Kong',
