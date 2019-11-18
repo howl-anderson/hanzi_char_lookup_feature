710f2a7e54e8effe4068dfad526d9081053c4d7e
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-18 16:15:51 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='hanzi_char_lookup_feature',
-    version='0.1',
+    name="hanzi_char_lookup_feature",
+    version="0.1",
     packages=[
-        'hanzi_char_lookup_feature',
-        'hanzi_char_lookup_feature.n_gram_lookup',
-        'hanzi_char_lookup_feature.n_gram_match',
-        'hanzi_char_lookup_feature.n_grams'
+        "hanzi_char_lookup_feature",
+        "hanzi_char_lookup_feature.n_gram_lookup",
+        "hanzi_char_lookup_feature.n_gram_match",
+        "hanzi_char_lookup_feature.n_grams",
     ],
-    url='',
-    license='',
-    author='Xiaoquan Kong',
-    author_email='',
-    description='',
-    install_requires=['pygtrie', 'tokenizer_tools']
+    url="https://github.com/howl-anderson/hanzi_char_lookup_feature",
+    license="MIT",
+    author="Xiaoquan Kong",
+    author_email="u1mail2me@gmail.com",
+    description="Dictionary Feature extractor for Deep learning",
+    install_requires=["pygtrie", "tokenizer_tools", "numpy"],
 )
