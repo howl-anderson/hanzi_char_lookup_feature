c3399199e006e0f53eca3762af3467ebd6250bfa
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-18 16:38:35 +0800

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
+import setuptools
 from setuptools import setup
 
 setup(
     name="hanzi_char_lookup_feature",
     version="0.1",
-    packages=[
-        "hanzi_char_lookup_feature",
-        "hanzi_char_lookup_feature.n_gram_lookup",
-        "hanzi_char_lookup_feature.n_gram_match",
-        "hanzi_char_lookup_feature.n_grams",
-    ],
+    packages=setuptools.find_packages(),
     url="https://github.com/howl-anderson/hanzi_char_lookup_feature",
     license="MIT",
     author="Xiaoquan Kong",
